a0e90c383a41e696ffb0d39607f9469ba1a15ec0
Enrico-eng <fahrideveci@gmail.com>
2023-02-23 10:23:09 +0300

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author:Fahri
+ *         fahrideveci@gmail.com
+ * Date:2023 / 02 /22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello I'm Fahri!\n");
+  printf("Major is MIS!\n");
+
+  return 0;
+}
